c3d70b0af994cd596636d4191f88134483318305
PascalA79 <arespascal79@gmail.com>
2022-09-20 15:16:32 -0400

    correction exemple ConnectionMYSQL

--- myapp/class/ConnectionMYSQL.js
+++ myapp/class/ConnectionMYSQL.js
@@ -8,11 +8,11 @@ class ConnectionMYSQL{
             user: user,
             password: password,
             database :database
-          });}
+          });
+          this.connectionMySQL.connect()
+        }
     excecute(query,fonction_error_results_fields){
-        this.connectionMySQL.connect();
         this.connectionMySQL.query(query, fonction_error_results_fields)
-        this.connectionMySQL.end();
     }
     async excecuteSync(query){
         let promiseResult= new Promise((resolve, reject)=>this.excecute(query,(error,results)=>{
@@ -20,6 +20,9 @@ class ConnectionMYSQL{
             resolve(results)
         }))
         return promiseResult;
+    }
+    end(){
+        this.connectionMySQL.destroy();
     }
 }
 module.exports = ConnectionMYSQL;
--- myapp/routes/api.js
+++ myapp/routes/api.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 var Session=require('../class/Session');
 var ConnectionMYSQL=require('../class/ConnectionMYSQL');
+// hide those values
+var session=new Session(router);
 const connectionMySQL= new ConnectionMYSQL({
   host: "173.176.94.124",
   user: "projet",
   password: "qwerty12345",
   database :"projet"
 })
-var session=new Session(router);
+
 /*connectionMySQL.excecute('SELECT * FROM Utilisateurs',(error,results,fields)=>{
   console.log(error)
   console.log(results);
@@ -18,18 +20,27 @@ var session=new Session(router);
 //router.get('/', (req, res, next)=>res.send('respond with a resource'));
 router.get('/*', appelApi);
 async function appelApi(req, res, next){
-//TODO droit d'accès avec la session
+  //TODO droit d'accès avec la session
+
+
   let arrayPath=req.baseUrl.split('/')
-  console.log(Object.values(arrayPath))
-  console.log(Object.values(arrayPath[1]))
-  if(Object.values(arrayPath).length==4){
-    let champs=arrayPath[2].toLowerCase()
-    let action=arrayPath[3].toLowerCase()
-    console.log(champs,action)
-    console.log(req.query)
-   let results=await connectionMySQL.excecuteSync('SELECT * FROM Utilisateurs');
-   console.log(results)
+  for(let index=0;index<arrayPath.length;index++)
+  {
+    if(arrayPath[0]!='api') arrayPath.shift()
+    else break
   }
+  let results=await connectionMySQL.excecuteSync('SELECT * FROM Utilisateurs');
+  console.log(results)
+  connectionMySQL.excecute('SELECT * FROM Utilisateurs',(error,r,fiellds)=>console.log(r))
+  console.log(Object.values(arrayPath))
+  if(Object.values(arrayPath).length==3){
+    let action=arrayPath[1].toLowerCase()
+    let champs=arrayPath[2].toLowerCase()
+    
+    console.log(action,champs)
+    console.log(req.query)
+  }
+  //connectionMySQL.end()
   res.send('respond with a resource');
 }
 
