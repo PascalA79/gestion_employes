5d5c994e46cbb94a69e6ce19a32cb378e2b0de03
PascalA79 <arespascal79@gmail.com>
2022-09-21 15:17:55 -0400

    redirection automatique vers login

--- myapp/class/Redirect.js
+++ myapp/class/Redirect.js
@@ -3,7 +3,7 @@ class Redirect{
         this.session=session;
         this.response=response
     }
-    // se connecter à la page cible si expr est true 
+    // se connecter à la page cible si expr est true
     // access retourne 0 dans ce cas, sinon 1
     access(key,expr=(value)=>!value,cible='/login'){
         if(expr(this.session.get(key))){
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,12 +4,19 @@ var Session=require('../class/Session')
 var Redirect=require('../class/Redirect')
 /* GET home page. */
 var session=new Session(router);
+//redirection automatique vers /login si non connecté
+router.use('/', function(req, res, next) {
+    session.start(req);
+    let redirect= new Redirect(session,res);
+    console.log(req.path)
+    if(req.path=='/login' || redirect.access('user')){
+      next();
+    }
+  })
+
 router.get(['/','/index'], function(req, res, next) {
   session.start(req);
-  let redirect= new Redirect(session,res)
-  if(redirect.access('user')){
     res.render('index',{user:{alias:session.get('user')},alerts:{}});
-  }
   //res.render('index', { title: 'Express' });
 })
 module.exports = router;
