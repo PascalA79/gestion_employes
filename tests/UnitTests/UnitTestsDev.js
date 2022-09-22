8db27e915aa95b68d79d246da5820ed2dfa498f0
PhilippeCLeger <philippe-cosmo.leger@outlook.com>
2022-09-22 07:53:54 -0400

    Avancement login

--- tests/UnitTests/UnitTestsDev.js
+++ tests/UnitTests/UnitTestsDev.js
@@ -5,16 +5,25 @@ AllTests["login.js"] = require("../gui/html/scripts/login").unitTests;
 
 
 
-
+let errorsTotal = 0;
 Object.keys(AllTests).forEach(fileName =>{
-    console.log(`${fileName} Unit tests`);
-    console.log("_________________________________________");
+    let errorsFile = 0;
     let unitTests = AllTests[fileName];
     Object.keys(unitTests).forEach(functionName => {
+        let errorsFunction = 0;
         let functionTests = unitTests[functionName];
         Object.keys(functionTests).forEach(testCase => {
-            console.log(`${functionTests[testCase]() ? "OK  " : "FAIL"}:: ${functionName} - ${testCase}`)
-        })
+            let result = functionTests[testCase]()
+            if(!result){
+                console.log(`FAIL:: ${fileName} - ${functionName} - ${testCase}`);
+                errorsFunction++;
+                errorsFile++;
+                errorsTotal++;
+            }
+        });
+        if(errorsFunction > 0)
+            console.log(`${functionName} errors: ${errorsFunction}`)
     })
-    console.log("_________________________________________");
+    console.log(`${fileName} errors: ${errorsFile}`)
 });
+console.log(`TOTAL ERRORS: ${errorsTotal}`);
--- tests/gui/html/scripts/login.js
+++ tests/gui/html/scripts/login.js
@@ -13,44 +13,62 @@ const ERROR_CODES={
     }
 }
 
-const ERROR_TEXTS = {}
-ERROR_TEXTS.USERNAME={}
-ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.OK] = "";
-ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.TOO_SHORT] = `Nom d'utilisateur trop court. Doit contenir au moins ${MIN_LENGTH_USERNAME} caractères.`;
-ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.TOO_LONG] = `Nom d'utilisateur trop court. Doit contenir au moins ${MIN_LENGTH_USERNAME} caractères.`;;
-ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.INVALID_CHAR] = "";
-
-
 const MIN_LENGTH_USERNAME = 5;
 const MAX_LENGTH_USERNAME = 45;
 const MIN_LENGTH_PASSWORD = 10;
 const MAX_LENGTH_PASSWORD = 45;
 
+const ERROR_TEXTS = {};
+ERROR_TEXTS.USERNAME={};
+ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.OK] = "";
+ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.TOO_SHORT] = `Nom d'utilisateur trop court. Doit contenir au moins ${MIN_LENGTH_USERNAME} caractères.`;
+ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.TOO_LONG] = `Nom d'utilisateur trop long. Doit contenir au plus ${MAX_LENGTH_USERNAME} caractères.`;;
+ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.INVALID_CHAR] = "Nom d'utilisateur ne doit pas contenir de caractères spéciaux";
+
+ERROR_TEXTS.PASSWORD={};
+ERROR_TEXTS.PASSWORD[ERROR_CODES.PASSWORD.OK] = "";
+ERROR_TEXTS.PASSWORD[ERROR_CODES.PASSWORD.TOO_SHORT] = `Mot de passe trop court. Doit contenir au moins ${MIN_LENGTH_PASSWORD} caractères.`;
+ERROR_TEXTS.PASSWORD[ERROR_CODES.PASSWORD.TOO_LONG] = `Mot de passe trop long. Doit contenir au plus ${MAX_LENGTH_PASSWORD} caractères.`;;
+ERROR_TEXTS.PASSWORD[ERROR_CODES.PASSWORD.NEEDS_VARIETY] = "Mot de passe doit contenir au moins une lettre majuscule et un chiffre";
+
+
+const REGEXP_TEMPLATE_USERNAME = "^\[A-Za-z0-9_]{" + MIN_LENGTH_USERNAME + "," + MAX_LENGTH_USERNAME + "}$";
+const REGEXP_TEMPLATE_PASSWORD = "^(?=.*[A-Z]+)(?=.*[0-9]+).{" + MIN_LENGTH_PASSWORD + "," + MAX_LENGTH_PASSWORD + "}$";
+const REGEXP_USERNAME = new RegExp(REGEXP_TEMPLATE_USERNAME);
+const REGEXP_PASSWORD = new RegExp(REGEXP_TEMPLATE_PASSWORD);
+
+function getValidationText(text, validateText, error_texts) {
+    return error_texts[validateText(text)];
+}
+
 function validateUsername(username){
     if(username.length < MIN_LENGTH_USERNAME) return ERROR_CODES.USERNAME.TOO_SHORT;
     if(username.length > MAX_LENGTH_USERNAME) return ERROR_CODES.USERNAME.TOO_LONG;
-    if(!new RegExp(`^\[A-Za-z0-9_]{${MIN_LENGTH_USERNAME},${MAX_LENGTH_USERNAME}}$`).test(username)) return ERROR_CODES.USERNAME.INVALID_CHAR;
+    if(!REGEXP_USERNAME.test(username)) return ERROR_CODES.USERNAME.INVALID_CHAR;
     return ERROR_CODES.USERNAME.OK;
 }
 
-function getUsernameError(username){
-    const USERCODES = ERROR_CODES.USERNAME;
-    switch(validateUsername){
-        case USERCODES.OK: return "";
-        case USERCODES.TOO_SHORT: return "Le nom d'utilisateur dont comporter au moins 5 caractères"
-    }
+function getUsernameValidationText(username){
+    return getValidationText(username, validateUsername, ERROR_TEXTS.USERNAME);
 }
 
 function validatePassword(password){
     if(password.length < 10) return ERROR_CODES.PASSWORD.TOO_SHORT;
     if(password.length > 45) return ERROR_CODES.PASSWORD.TOO_LONG;
-    if(!new RegExp(`^(?=.*[A-Z]+)(?=.*[0-9]+).{${MIN_LENGTH_PASSWORD},${MAX_LENGTH_PASSWORD}}$`).test(password)) return ERROR_CODES.PASSWORD.NEEDS_VARIETY;
+    if(!REGEXP_PASSWORD.test(password)) return ERROR_CODES.PASSWORD.NEEDS_VARIETY;
     return ERROR_CODES.PASSWORD.OK;
 }
 
+function getPasswordValidationText(username){
+    return getValidationText(username, validatePassword, ERROR_TEXTS.PASSWORD);
+}
+
 module.exports.ERROR_CODES = ERROR_CODES;
-module.exports.validateUsername = validateUsername
-module.exports.validatePassword = validatePassword
+module.exports.ERROR_TEXTS = ERROR_TEXTS;
+module.exports.validateUsername = validateUsername;
+module.exports.validatePassword = validatePassword;
+module.exports.getUsernameValidationText = getUsernameValidationText;
+module.exports.getPasswordValidationText = getPasswordValidationText;
 
 
 
@@ -65,12 +83,25 @@ const unitTests = {
         "TOO_LONG": () => validateUsername("asdasdasdasdasdasdasdasdasdasdasdasdasdasdasda") == ERROR_CODES.USERNAME.TOO_LONG,
         "INVALID_CHAR": () => validateUsername("asdasd@") == ERROR_CODES.USERNAME.INVALID_CHAR
     },
+    "getUsernameValidationText": {
+        "OK": () => getUsernameValidationText("asd_asd") == ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.OK],
+        "TOO_SHORT": () => getUsernameValidationText("asda") == ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.TOO_SHORT],
+        "TOO_LONG": () => getUsernameValidationText("asdasdasdasdasdasdasdasdasdasdasdasdasdasdasda") == ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.TOO_LONG],
+        "INVALID_CHAR": () => getUsernameValidationText("asdasd@") == ERROR_TEXTS.USERNAME[ERROR_CODES.USERNAME.INVALID_CHAR]
+    },
     "validatePassword": {
         "OK": () => validatePassword("Password123") == ERROR_CODES.PASSWORD.OK,
         "TOO_SHORT": () => validatePassword("Password1") == ERROR_CODES.PASSWORD.TOO_SHORT,
         "TOO_LONG": () => validatePassword("Password1Password1Password1Password1Password12") == ERROR_CODES.PASSWORD.TOO_LONG,
         "MISSING_NUMBER": () => validatePassword("Passwordasdfasd") == ERROR_CODES.PASSWORD.NEEDS_VARIETY,
         "MISSING_CAPITAL_LETTER": () => validatePassword("passwordasdfasd123") == ERROR_CODES.PASSWORD.NEEDS_VARIETY
+    },
+    "getPasswordValidationText": {
+        "OK": () => getPasswordValidationText("Password123") == ERROR_TEXTS.PASSWORD[ERROR_CODES.PASSWORD.OK],
+        "TOO_SHORT": () => getPasswordValidationText("Password1") == ERROR_TEXTS.PASSWORD[ERROR_CODES.PASSWORD.TOO_SHORT],
+        "TOO_LONG": () => getPasswordValidationText("Password1Password1Password1Password1Password12") == ERROR_TEXTS.PASSWORD[ERROR_CODES.PASSWORD.TOO_LONG],
+        "MISSING_NUMBER": () => getPasswordValidationText("Passwordasdfasd") == ERROR_TEXTS.PASSWORD[ERROR_CODES.PASSWORD.NEEDS_VARIETY],
+        "MISSING_CAPITAL_LETTER": () => getPasswordValidationText("passwordasdfasd123") == ERROR_TEXTS.PASSWORD[ERROR_CODES.PASSWORD.NEEDS_VARIETY]
     }
 }
 module.exports.unitTests = unitTests;
