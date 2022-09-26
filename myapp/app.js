8c4668ba750fa0c8d4f07ad3a70d2cb706adc1ad
PhilippeCLeger <philippe-cosmo.leger@outlook.com>
2022-09-26 16:32:22 -0400

    Ajout d'horaire-perso et finalisation de navbar et index

--- myapp/app.js
+++ myapp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 var loginRouter = require('./routes/login');
 var plancherRouter = require('./routes/plancher');
+var horairePersoRouter = require('./routes/horaire_perso');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/api/', apiRouter);
 app.use('/login', loginRouter);
 app.use('/plancher', plancherRouter);
+app.use('/horaire-perso', horairePersoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ myapp/routes/horaire_perso.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var Session=require('../class/Session')
+var Redirect=require('../class/Redirect')
+/* GET home page. */
+var session=new Session(router);
+
+  router.get(['/','/index'], function(req, res, next) {
+    session.start(req);
+    res.render('horaire-perso',{user:{alias:session.get('user')},alerts:{}});
+  })
+
+module.exports = router;
